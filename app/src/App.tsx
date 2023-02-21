e3fd473c4c9bb922e45900f5581e745b4de4f3dc
Dakshesh Jain <dakshesh.jain14@gmail.com>
2023-02-21 23:07:26 +0530

    dev: editor setup

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,16 @@
+import Editor from "./components/editor";
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello World</h1>;
+  return (
+    <div className="container mx-auto py-10">
+      <h1 className="text-2xl mb-5">Type something in real-time:</h1>
+      <Editor
+        onChange={(value) => {
+          console.log(value);
+        }}
+      />
+    </div>
+  );
 };
 
 export default App;
---
+++ app/src/components/editor/index.tsx
@@ -0,0 +1,87 @@
+import { useCallback, FC, useState, useEffect } from "react";
+import { InvalidContentHandler } from "remirror";
+import {
+  BoldExtension,
+  ItalicExtension,
+  CalloutExtension,
+  PlaceholderExtension,
+  CodeBlockExtension,
+  CodeExtension,
+  HistoryExtension,
+  LinkExtension,
+  UnderlineExtension,
+  HeadingExtension,
+  OrderedListExtension,
+  ListItemExtension,
+  BulletListExtension,
+  DropCursorExtension,
+  StrikeExtension,
+  MentionAtomExtension,
+  FontSizeExtension,
+} from "remirror/extensions";
+import {
+  Remirror,
+  useRemirror,
+  EditorComponent,
+  OnChangeJSON,
+} from "@remirror/react";
+
+export interface IRemirrorRichTextEditor {
+  onChange: (value: any) => void;
+  value?: any;
+}
+
+const defaultValue = "<h1>Hello World</h1>";
+
+const Editor: FC<IRemirrorRichTextEditor> = (props) => {
+  const { onChange, value = defaultValue } = props;
+
+  // remirror error handler
+  const onError: InvalidContentHandler = useCallback(
+    ({ json, invalidContent, transformers }: any) => {
+      // Automatically remove all invalid nodes and marks.
+      return transformers.remove(json, invalidContent);
+    },
+    []
+  );
+
+  // remirror manager
+  const { manager, state } = useRemirror({
+    extensions: () => [
+      new BoldExtension(),
+      new ItalicExtension(),
+      new UnderlineExtension(),
+      new HeadingExtension({ levels: [1, 2, 3] }),
+      new FontSizeExtension({ defaultSize: "16", unit: "px" }),
+      new OrderedListExtension(),
+      new ListItemExtension(),
+      new BulletListExtension({ enableSpine: true }),
+      new CalloutExtension({ defaultType: "warn" }),
+      new CodeBlockExtension(),
+      new CodeExtension(),
+      new HistoryExtension(),
+      new LinkExtension({ autoLink: true }),
+    ],
+    content: value,
+    selection: "start",
+    stringHandler: "html",
+    onError,
+  });
+
+  return (
+    <div className="mt-2 mb-4">
+      <Remirror
+        manager={manager}
+        initialContent={state}
+        classNames={["p-4 focus:outline-none"]}
+      >
+        <div className="rounded-md border">
+          <EditorComponent />
+          <OnChangeJSON onChange={onChange} />
+        </div>
+      </Remirror>
+    </div>
+  );
+};
+
+export default Editor;
