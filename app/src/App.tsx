971023425aea17e4c119b4f07f86743b7d4874d6
Dakshesh Jain <dakshesh.jain14@gmail.com>
2023-02-22 17:49:07 +0530

    feat: changes on one tab will be shown in other tabs as well

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,29 +1,33 @@
-import Editor from "./components/editor";
+import { useEffect, useRef } from "react";
+
+// remirror
+import { EmptyShape, Extension } from "remirror";
+import { ReactFrameworkOutput } from "@remirror/react";
 
 // hooks
 import useWebSocket from "./hooks/use-websocket";
+
+// components
+import Editor from "./components/editor";
 
 const App = () => {
   const { messages, sendMessage } = useWebSocket(
     "ws://localhost:8000/ws/editor/my-room/"
   );
 
+  const editorRef = useRef<ReactFrameworkOutput<Extension<EmptyShape>> | null>(
+    null
+  );
+
+  useEffect(() => {
+    if (!editorRef.current) return;
+    editorRef.current.setContent(messages);
+  }, [messages, editorRef]);
+
   return (
     <div className="container mx-auto py-10">
       <h1 className="text-2xl mb-5">Type something in real-time:</h1>
-      <textarea
-        value={messages}
-        onChange={(e) => {
-          sendMessage(e.target.value);
-        }}
-      >
-        {messages}
-      </textarea>
-      {/* <Editor
-        onChange={(value) => {
-          sendMessage(value);
-        }}
-      /> */}
+      <Editor value={messages} onChange={sendMessage} ref={editorRef} />
     </div>
   );
 };
--- app/src/components/editor/index.tsx
+++ app/src/components/editor/index.tsx
@@ -1,5 +1,8 @@
-import { useCallback, FC } from "react";
-import { InvalidContentHandler } from "remirror";
+import { forwardRef, useCallback, useImperativeHandle } from "react";
+
+import "remirror/styles/all.css";
+
+import { Extension, InvalidContentHandler } from "remirror";
 import {
   BoldExtension,
   ItalicExtension,
@@ -21,64 +24,69 @@ import {
   useRemirror,
   EditorComponent,
   OnChangeJSON,
+  ReactFrameworkOutput,
 } from "@remirror/react";
 
-export interface IRemirrorRichTextEditor {
+export interface Props {
   onChange: (value: any) => void;
   value?: any;
 }
 
-const defaultValue = "<h1>Hello World</h1>";
+export const extensions = () => [
+  new BoldExtension(),
+  new ItalicExtension(),
+  new UnderlineExtension(),
+  new HeadingExtension({ levels: [1, 2, 3] }),
+  new FontSizeExtension({ defaultSize: "16", unit: "px" }),
+  new OrderedListExtension(),
+  new ListItemExtension(),
+  new BulletListExtension({ enableSpine: true }),
+  new CalloutExtension({ defaultType: "warn" }),
+  new CodeBlockExtension(),
+  new CodeExtension(),
+  new HistoryExtension(),
+  new LinkExtension({ autoLink: true }),
+];
 
-const Editor: FC<IRemirrorRichTextEditor> = (props) => {
-  const { onChange, value = defaultValue } = props;
+const EditorWithRef = forwardRef<ReactFrameworkOutput<Extension>, Props>(
+  (props, ref) => {
+    const { onChange, value } = props;
 
-  // remirror error handler
-  const onError: InvalidContentHandler = useCallback(
-    ({ json, invalidContent, transformers }: any) => {
-      // Automatically remove all invalid nodes and marks.
-      return transformers.remove(json, invalidContent);
-    },
-    []
-  );
+    // remirror error handler
+    const onError: InvalidContentHandler = useCallback(
+      ({ json, invalidContent, transformers }: any) => {
+        // Automatically remove all invalid nodes and marks.
+        return transformers.remove(json, invalidContent);
+      },
+      []
+    );
 
-  // remirror manager
-  const { manager, state } = useRemirror({
-    extensions: () => [
-      new BoldExtension(),
-      new ItalicExtension(),
-      new UnderlineExtension(),
-      new HeadingExtension({ levels: [1, 2, 3] }),
-      new FontSizeExtension({ defaultSize: "16", unit: "px" }),
-      new OrderedListExtension(),
-      new ListItemExtension(),
-      new BulletListExtension({ enableSpine: true }),
-      new CalloutExtension({ defaultType: "warn" }),
-      new CodeBlockExtension(),
-      new CodeExtension(),
-      new HistoryExtension(),
-      new LinkExtension({ autoLink: true }),
-    ],
-    content: value,
-    selection: "start",
-    stringHandler: "html",
-    onError,
-  });
+    // remirror manager
+    const { manager, state, getContext } = useRemirror({
+      extensions,
+      content: value,
+      selection: "start",
+      stringHandler: "html",
+      onError,
+    });
 
-  return (
-    <div className="mt-2 mb-4">
-      <Remirror
-        manager={manager}
-        initialContent={state}
-        classNames={["p-4 focus:outline-none"]}
-      >
-        <div className="rounded-md border">
-          <EditorComponent />
-          <OnChangeJSON onChange={onChange} />
-        </div>
-      </Remirror>
-    </div>
-  );
-};
+    useImperativeHandle(ref, () => getContext() as any, [getContext]);
 
-export default Editor;
+    return (
+      <div className="mt-2 mb-4">
+        <Remirror
+          manager={manager}
+          initialContent={state}
+          classNames={["p-4 focus:outline-none"]}
+        >
+          <div className="rounded-md border">
+            <EditorComponent />
+            <OnChangeJSON onChange={onChange} />
+          </div>
+        </Remirror>
+      </div>
+    );
+  }
+);
+
+export default EditorWithRef;
